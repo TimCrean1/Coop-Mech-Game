10b3938aa8bcbcef7e191a89bc94fc24a2ce8b7b
Liam <lking122@outlook.com>
2025-11-01 15:16:33 -0600

    Update ExplosionVFX.cs

--- Coop-Mech-Game/Assets/Scripts/Player/VFX/ExplosionVFX.cs
+++ Coop-Mech-Game/Assets/Scripts/Player/VFX/ExplosionVFX.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class ExplosionVFX : MonoBehaviour
 {
-    private void OnEnable()
+    private void Start()
     {
         StartCoroutine(DestroyRoutine());
     }
 
     private IEnumerator DestroyRoutine()
     {
-        yield return new WaitForSeconds(10f);
+        yield return new WaitForSeconds(15f);
+        Destroy(gameObject);
     }
 }
