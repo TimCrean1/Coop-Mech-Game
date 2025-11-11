062ac3833c9db1284923a9bd598465723916544c
”TimCrean” <tim.c.crean@gmail.com>
2025-11-11 15:32:38 -0700

    Liam took my computer :(

--- Coop-Mech-Game/Assets/Scripts/Map/ActivateArea.cs
+++ Coop-Mech-Game/Assets/Scripts/Map/ActivateArea.cs
@@ -70,11 +70,29 @@ public class ActivateArea : MonoBehaviour
 
     private IEnumerator Activate()
     {
-        foreach (GameObject go in toActivate)
+        for(int i = 0; i<toActivate.Count; i++)
         {
-            go.SetActive(true);
+            MovingAlien ma = toActivate[i].GetComponent<MovingAlien>();
+            SentryAlien sa = toActivate[i].GetComponent<SentryAlien>();
+
+            if(ma != null) 
+            {
+                //Debug.Log("ma" + i);
+
+                ma.player = player;
+                ma.gameObject.SetActive(true);
+                StartCoroutine(ma.AqcuireTarget());
+            }
+            else if(sa != null) 
+            {
+                //Debug.Log("sa" + i);
+
+                sa.player = player;
+                sa.gameObject.SetActive(true);
+                StartCoroutine(sa.FireRoutine());
+            }
+
             yield return null;
         }
-        yield return null;
     }
 }
