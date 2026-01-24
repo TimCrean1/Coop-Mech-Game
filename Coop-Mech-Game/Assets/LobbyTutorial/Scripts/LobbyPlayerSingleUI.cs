9394373325074e83ed844d5af121cfbf14f0dec3
nansan4 <nando@santosalliances.com>
2026-01-23 17:09:01 -0700

    lobby system moves players

--- Coop-Mech-Game/Assets/LobbyTutorial/Scripts/LobbyPlayerSingleUI.cs
+++ Coop-Mech-Game/Assets/LobbyTutorial/Scripts/LobbyPlayerSingleUI.cs
@@ -29,7 +29,7 @@ public class LobbyPlayerSingleUI : MonoBehaviour {
         playerNameText.text = player.Data[LobbyManager.KEY_PLAYER_NAME].Value;
         LobbyManager.PlayerCharacter playerCharacter = 
             System.Enum.Parse<LobbyManager.PlayerCharacter>(player.Data[LobbyManager.KEY_PLAYER_CHARACTER].Value);
-        characterImage.sprite = LobbyAssets.Instance.GetSprite(playerCharacter);
+        //characterImage.sprite = LobbyAssets.Instance.GetSprite(playerCharacter);
     }
 
     private void KickPlayer() {
