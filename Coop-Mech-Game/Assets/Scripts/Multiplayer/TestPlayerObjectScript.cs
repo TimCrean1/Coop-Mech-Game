7c8270834303255c10ee839b1e8173f2f5f360a3
nansan4 <nando@santosalliances.com>
2026-01-25 14:27:55 -0700

    network variable 2

--- Coop-Mech-Game/Assets/Scripts/Multiplayer/TestPlayerObjectScript.cs
+++ Coop-Mech-Game/Assets/Scripts/Multiplayer/TestPlayerObjectScript.cs
@@ -6,8 +6,11 @@ public class TestPlayerObjectScript : NetworkBehaviour
 {
     public PlayerController playerController;
     [SerializeField]private bool isPlayerOne;
-    //private Vector2 mousePos;
-    private NetworkVariable<Vector2> mousePos = new NetworkVariable<Vector2>();
+    private Vector2 mousePos;
+    private float mouseX;
+    private float mouseY;
+    
+    private NetworkVariable<Vector2> mouseNetPos = new NetworkVariable<Vector2>();
     private PlayerInputActions playerInputActions;
 
     public override void OnNetworkSpawn()
@@ -102,15 +105,15 @@ public class TestPlayerObjectScript : NetworkBehaviour
         mousePos = Input.mousePosition;
         mousePos.x = mousePos.x/Screen.width;
         mousePos.y = mousePos.y/Screen.height;
-
+        mouseNetPos.Value = mousePos;
         // Send mouse position to PlayerController
         if (isPlayerOne)
         {
-            playerController.ProcessMouse1Input(mousePos);
+            playerController.ProcessMouse1Input(mouseNetPos.Value);
         }
         else
         {
-            playerController.ProcessMouse2Input(mousePos);
+            playerController.ProcessMouse2Input(mouseNetPos.Value);
         }
     }
 }
