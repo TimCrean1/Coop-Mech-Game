20bc473cc1d06824e3785a887a1619718dd1af6d
Liam <lking122@outlook.com>
2026-02-13 12:24:18 -0700

    script changes

--- Coop-Mech-Game/Assets/Scripts/Player/Weapons/Base/BaseWeapon.cs
+++ Coop-Mech-Game/Assets/Scripts/Player/Weapons/Base/BaseWeapon.cs
@@ -21,7 +21,7 @@ public abstract class BaseWeapon : MonoBehaviour
     [SerializeField] private int ammo = 10;
     [SerializeField] private float baseFireRate = 1f;
     [SerializeField] private float cooldownTime = 1.0f;
-    [SerializeField] private float damage = 5;
+    [SerializeField] private float damage = 50;
     [SerializeField] private Vector3 maxRotationAxes = Vector3.zero;
 
     [Header("READ ONLY")]
@@ -56,13 +56,16 @@ public abstract class BaseWeapon : MonoBehaviour
             Physics.Raycast(muzzle.position, muzzle.forward, out hit);
             if (muzzleComp) { muzzleComp.SendFireEvent(); }
 
-            if (hit.collider.CompareTag("Player"))
+            if (hit.collider.gameObject.CompareTag("TeamOne"))
             {
-                
                 // get team-specific info and send to wherever we're handling the health of the teams
+                GameManager.Instance.DamageTeamRpc(1, damage);
             }
-
-            else if (hit.collider.CompareTag("Target"))
+            else if (hit.collider.gameObject.CompareTag("TeamTwo"))
+            {
+                GameManager.Instance.DamageTeamRpc(2, damage);
+            }
+            else if (hit.collider.gameObject.CompareTag("Target"))
             {
                 Debug.Log("Hit!");
                 hit.collider.gameObject.SetActive(false);
