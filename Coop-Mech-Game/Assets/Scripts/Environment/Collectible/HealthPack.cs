c1d5c2bb13fa54b7e76ab4ca3201da43af5e3317
Liam <lking122@outlook.com>
2026-02-22 18:32:52 -0700

    helth agen

--- Coop-Mech-Game/Assets/Scripts/Environment/Collectible/HealthPack.cs
+++ Coop-Mech-Game/Assets/Scripts/Environment/Collectible/HealthPack.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class HealthPack : MonoBehaviour
 {
     [SerializeField] private float healAmount = 150f;
     [SerializeField] private BoxCollider coll;
+    [SerializeField] private List<Vector3> locations = new List<Vector3>();
+    [SerializeField] private float cooldown = 10f;
+
+    private int idx = 0;
+
+    private void Start()
+    {
+        StartCoroutine(NextPosRoutine());
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -11,10 +22,27 @@ public class HealthPack : MonoBehaviour
         {
             Debug.Log("Collided with team one!");
             GameManager.Instance.HealTeamRpc(1, healAmount); //team, health
+
+            StartCoroutine(NextPosRoutine());
+
         } else if (collision.gameObject.CompareTag("TeamTwo"))
         {
             Debug.Log("Collided with team two!");
             GameManager.Instance.HealTeamRpc(2, healAmount);
+
+            StartCoroutine(NextPosRoutine());
         }
+    }
+
+    private IEnumerator NextPosRoutine()
+    {
+        gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(cooldown);
+
+        transform.position = locations[idx];
+        idx = (idx + 1) % locations.Count;
+
+        gameObject.SetActive(true);
     }
 }
