18d38793448b3b0c7ad4b8453a18ad225d2c35fd
Liam <lking122@outlook.com>
2026-03-05 10:47:03 -0700

    list extensions documentation

--- Coop-Mech-Game/Assets/Scripts/Class Extensions/ListExtensions.cs
+++ Coop-Mech-Game/Assets/Scripts/Class Extensions/ListExtensions.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 
 public static class ListExtensions
 {
+    /// <summary>
+    /// Shuffles the elements of a given list, modifies base list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
     public static void Shuffle<T>(this List<T> list)
     {
         for (int i = 0; i < list.Count; i++)
@@ -17,8 +22,26 @@ public static class ListExtensions
         }
     }
 
+    /// <summary>
+    /// Checks if an index is within the bounds of the given list and if the list is not null
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="index"></param>
+    /// <param name="list"></param>
+    /// <returns> a bool if the above conditions are all true</returns>
     public static bool IsValidIndex<T>(this int index, IList<T> list)
     {
         return list != null && index >=0 && index < list.Count;
+    }
+
+    /// <summary>
+    /// Gets and returns the last item in a list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
+    /// <returns> the last item in a list, type agnostic</returns>
+    public static T GetLastItem<T>(this List<T> list)
+    {
+        return list[list.Count - 1];
     }
 }
