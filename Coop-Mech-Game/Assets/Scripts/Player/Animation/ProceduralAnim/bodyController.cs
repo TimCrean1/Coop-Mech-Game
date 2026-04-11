897395ff67aa2fadda2e1154a66646dc9a8e5a00
nansan4 <nando@santosalliances.com>
2026-04-11 17:21:00 -0600

    redo rig

--- Coop-Mech-Game/Assets/Scripts/Player/Animation/ProceduralAnim/bodyController.cs
+++ Coop-Mech-Game/Assets/Scripts/Player/Animation/ProceduralAnim/bodyController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,13 +6,15 @@ public class bodyController : MonoBehaviour
 {
 
     public GameObject[] legTargets;
-    public GameObject[] letCubes;
+    public GameObject[] legCubes;
     Vector3[] legPositions;
     Vector3[] legOriginalPositions;
 
+    public float maxMoveDist = 2.5f;
+    public int legMovementSmoothness = 4;
 
     List<int> nextIndexToMove = new List<int>();
-    List<int> currentlyMoving = new List<int>();
+    List<int> indexMoving = new List<int>();
 
     private void Start()
     {
@@ -33,7 +36,51 @@ public class bodyController : MonoBehaviour
     {
         for(int i = 0; i < legTargets.Length; i++)
         {
-            legTargets[i].transform.position = legOriginalPositions[i];
+
+            if (Vector3.Distance(legTargets[i].transform.position, legCubes[i].transform.position) >= maxMoveDist)
+            {
+                if (!nextIndexToMove.Contains(i) && !indexMoving.Contains(i))
+                {
+                    nextIndexToMove.Add(i);
+
+                }
+                else if (!indexMoving.Contains(i))
+                {
+                    legTargets[i].transform.position = legOriginalPositions[i];
+                }
+            }
+        }
+        if (nextIndexToMove.Count == 0 || indexMoving.Count != 0) {
+            return;
+        }
+
+        Vector3 targetPosition = legCubes[nextIndexToMove[0]].transform.position;
+        StartCoroutine(Step(nextIndexToMove[0], targetPosition));
+    }
+
+    IEnumerator Step(int index, Vector3 moveTo)
+    {
+        if (nextIndexToMove.Contains(index))
+        {
+            nextIndexToMove.Remove(index);
+        }
+        if (!indexMoving.Contains(index))
+        {
+            indexMoving.Add(index);
+        }
+
+        Vector3 startingPosition = legOriginalPositions[index];
+
+        for(int i = 1; i <= legMovementSmoothness; i++)
+        {
+            legTargets[index].transform.position = Vector3.Lerp(startingPosition, moveTo, i / legMovementSmoothness);
+            yield return new WaitForFixedUpdate();
+        }
+
+        legOriginalPositions[index] = moveTo;
+
+        if (indexMoving.Contains(index)) {
+            indexMoving.Remove(index);
         }
     }
 }
